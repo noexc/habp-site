c314ae2c4b63a5f84b2a9d4a88012d18befb0ca2
Ricky Elrod <ricky@elrod.me>
2014-02-18 19:07:22 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
+
+if [ ! -f "$cwd/dist/build/habpsite/habpsite" ]; then
+  echo "habpsite binary not found."
+  exit 1
+fi
+
+rm -rf "$cwd/_site/" "$cwd/_cache/"
+
+"$cwd/dist/build/habpsite/habpsite" build && rsync -Havzre 'ssh' --delete _site/ w8upd.org:/srv/www/habp.w8upd.org/
