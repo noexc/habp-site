01ac17ada5530f89e8c5ccd1b45470580204882f
Ricky Elrod <ricky@elrod.me>
2014-02-18 19:22:01 -0500

    add preview script

---
+++ preview.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
+
+if [ ! -f "$cwd/dist/build/habpsite/habpsite" ]; then
+  echo "habpsite binary not found."
+  exit 1
+fi
+
+"$cwd/dist/build/habpsite/habpsite" watch
